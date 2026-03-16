f56937d94e0a077b4bfcc93eeb1cd0630ee6cc46
vkarthikeyan4392-web <vkarthikeyan4392@gmail.com>
2026-03-16 16:05:10 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,5 @@
-
+print("This is a jenkins build file !")
+print("##############################")
+print("Building jenkins pipeline ok !!!")
+print("################################")
+print("Thank you !!")
